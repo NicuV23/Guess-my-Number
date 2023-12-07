fdd27080224b9463676473ad8baef9646a70baf5
NicuV23 <v.nicu213@gmail.com>
2023-12-07 18:32:26 +0200

    debbuging

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ document.querySelector('.check').addEventListener('click', function () {
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
-    } else if (guess !== secretNumber) {
-      if (score > 1) {
-        document.querySelector('.message').textContent =
-          guess > secretNumber ? 'Too high' : 'Too Low';
-        score--;
-        document.querySelector('.score').textContent = score;
-      } else {
-        document.querySelector('.message').textContent = 'You lost the game';
-        document.querySelector('.score').textContent = 0;
-      }
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high' : 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
